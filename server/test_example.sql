193d28f2215e63fad220d7ef67bf45b6f4f7a944
MSI-90 <s.miller@list.ru>
2025-10-21 01:46:49 +0700

    Added .sql file with 3 record for event table for testing POST request

---
+++ server/test_example.sql
@@ -0,0 +1,7 @@
+-- This SQL code is suggested as an alternative to test the POST request endpoint.
+
+INSERT INTO events (name, total_seats)
+VALUES
+    ('Конференция по ИИ', 150),
+    ('Музыкальный фестиваль', 500),
+    ('Выставка современного искусства', 200);
