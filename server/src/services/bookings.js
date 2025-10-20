5b14cb73169e30057939d597d7577da0f600c32e
MSI-90 <s.miller@list.ru>
2025-10-21 01:41:46 +0700

    Finish dev

--- server/src/services/bookings.js
+++ server/src/services/bookings.js
@@ -4,12 +4,6 @@ const eventService = require('../services/events');
 const createBooking = async(eventId, user_id) => {
     try{
         const event = await eventService.findEventById(eventId);
-        if (!event) {
-            const error = new Error(`Event with ID ${eventId} not found`);
-            error.status = 404;
-            throw error;
-        }
-
         return await Booking.create({ event_id: event.id, user_id: user_id });
 
     } catch (error) {
--- server/src/services/events.js
+++ server/src/services/events.js
@@ -1,9 +1,16 @@
 const Events = require('../models/Event');
 
 const findEventById = async(eventId) => {
-    return await Events.findByPk(eventId);
+    const event = await Events.findByPk(eventId);
+    if (!event) {
+        const error = new Error(`Event with ID ${eventId} not found`);
+        error.status = 404;
+        throw error;
+    }
+
+    return event;
 };
 
 module.exports = {
-    findEventById,
+    findEventById
 }
